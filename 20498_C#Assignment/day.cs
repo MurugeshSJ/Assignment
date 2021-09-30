a9af08559882bf66aad0286b12685bfc9f15a9fa
murugesh <murugeshj56@gmail.com>
2021-09-30 08:01:46 +0530

    C# assignment Day_1

---
+++ 20498_C#Assignment/day.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    class holiday
+    {
+        static void Main1()
+        {
+            Console.WriteLine("enter day");
+            string day = Console.ReadLine();
+            int n = int.Parse(day);
+            if(n==0 || n > 7)
+            {
+                Console.WriteLine("invalid day");
+            }else if (n <= 5)
+            {
+                Console.WriteLine("working day");
+            }else
+                Console.WriteLine("holiday");
+        }
+    }
+}
